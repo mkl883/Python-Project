94af68f0c8d9a20ac49701a5a0b604bfd2ea17e2
mkl883 <mkl883@tamu.edu>
2018-02-27 21:46:12 -0600

    Working to open a file and print
    Continue on from here by answering question 2 parts

--- Project3.py
+++ Project3.py
@@ -1,19 +1,14 @@
-from urllib.request import urlretrieve
+import urllib
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
-# Use urlretrieve() to fetch a remote copy and save into the local file path
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
-
 FILE_NAME = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
 # Use open() to get a filehandle that can access the file
-fh = open(FILE_NAME)
+fh = urllib.urlopen(FILE_NAME)
 
 # Loop through the file 
 for line in fh:
 	print(line)
 
-
-# The loop example above is memory-efficient, and also easy to read
