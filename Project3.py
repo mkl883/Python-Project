14cb6939c753239c82abd5f89f1b17d7cf6bdb0f
mkl883 <mkl883@tamu.edu>
2018-02-27 21:40:36 -0600

    Changed from Professor's example

--- Project3.py
+++ Project3.py
@@ -1,6 +1,19 @@
-import urllib
+from urllib.request import urlretrieve
 
-link = urllib.urlopen('https://s3.amazonaws.com/tcmg476/http_access_log')
-lines = link.readlines()
-for line in lines:
-	print line
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+# Use urlretrieve() to fetch a remote copy and save into the local file path
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+FILE_NAME = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+
+# Use open() to get a filehandle that can access the file
+fh = open(FILE_NAME)
+
+# Loop through the file 
+for line in fh:
+	print(line)
+
+
+# The loop example above is memory-efficient, and also easy to read
