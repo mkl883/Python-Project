ba84fb27d029f6762967d24a633486b232af7ae4
mkl883 <mkl883@tamu.edu>
2018-02-28 17:24:49 -0600

    Counts total

--- Project3.py
+++ Project3.py
@@ -1,4 +1,5 @@
 import urllib
+import re
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
@@ -7,8 +8,14 @@ FILE_NAME = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 
 # Use open() to get a filehandle that can access the file
 fh = urllib.urlopen(FILE_NAME)
-
+print "This will take a while to compute, please stand by..."
 # Loop through the file 
+total = 0
 for line in fh:
-	print(line)
+	total += 1
+print "The total amount of requests is %d" % (total)
+
+	
+	
+
 
